b43662a47c84a8d5a5d5482aa7c0a75e308d1032
RAKHYUN <kyle2598@gmail.com>
2023-12-20 10:51:25 -0600

    Initial Commit

---
+++ ExpenseNote/ExpenseNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseNoteApp.swift
+//  ExpenseNote
+//
+//  Created by Rakhyun Kim on 12/20/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
